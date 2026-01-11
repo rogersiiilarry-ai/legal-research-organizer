f581bd58e5eb14e2c33e797bcd544eeea77d9fc7
Larry Rogers <rogersiiilarry@gmail.com>
2026-01-11 00:33:27 -0500

    ui: add shell layout for top nav

---
+++ frontend/app/(shell)/layout.tsx
@@ -0,0 +1,10 @@
+﻿import ShellHeader from "../../components/ShellHeader";
+
+export default function ShellLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <ShellHeader />
+      {children}
+    </>
+  );
+}
